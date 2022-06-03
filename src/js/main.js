94ca3e8d211cdcb292e446f4cc1abb522c4f532a
michal <michal.majda.11@gmail.com>
2022-06-03 20:39:23 +0200

    js script added - clicks work, counting work, closing work

---
+++ src/js/main.js
@@ -0,0 +1,58 @@
+const mainBtn = document.querySelector('.btn1')
+const info = document.querySelector('.info')
+const modal = document.querySelector('.modal-shadow')
+const close = document.querySelector('.fa-x')
+const modalTable = document.querySelector('.modal')
+const clearBtn = document.querySelector('.btn2')
+
+
+const clearCounter = () => {
+    sessionStorage.currentResult = 0
+    modal.style.display = "none"
+}
+
+const addClearButton = () => {
+    if(sessionStorage.currentResult >= 5) {
+        if(clearBtn.style.display != "block"){
+        clearBtn.style.display = "block"
+        }
+    }
+    else{
+        clearBtn.style.display = "none" 
+    }
+}
+
+const countAndOpenPopup = () => {
+    if(typeof(Storage) !== undefined){
+        if(sessionStorage.currentResult){
+            sessionStorage.currentResult = Number(sessionStorage.currentResult) + 1;
+        }
+        else {
+            sessionStorage.currentResult = 1
+        }
+    }
+
+    addClearButton()
+
+    info.textContent = `You have clicked ${sessionStorage.currentResult} times to related button.`
+    modal.style.display = "block"
+} 
+
+
+
+const closePopup = e => {
+    if(modal.style.display == "block"){
+        if(e.target == modal || e.target == close){
+            modal.style.display = "none"
+        }
+}
+}
+
+
+
+mainBtn.addEventListener('click', countAndOpenPopup)
+window.addEventListener('click', closePopup)
+clearBtn.addEventListener('click', clearCounter)
+
+
+
